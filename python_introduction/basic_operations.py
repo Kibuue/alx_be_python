ef05311180e56fdc4a8bc1ff109e025faf1a1a35
Esther Kibuu <kibuue@gmail.com>
2025-09-14 20:27:26 +0300

    Added, subtracted and Multiplied two numbers

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,10 @@
+number1 = 10
+number2 = 5
+
+addition = number1 + number2
+subtraction = number1 - number2
+multiplication = number1 * number2
+
+print(f{number1} + {number2} = {addition})
+print(f{number1} - {number2} = {subtraction})
+print(f{number1} basic_operations.py {number2} = {multiplication})
