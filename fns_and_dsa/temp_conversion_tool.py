5f92e3baff0e404965de9b494448b58d5f7be469
Esther Kibuu <kibuue@gmail.com>
2025-09-28 23:55:21 +0300

    Fixed a error message string to match the exact one used on ALX

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -24,8 +24,8 @@ def main():
         else:
             raise ValueError("Invalid unit. Please enter 'C' for Celsius or 'F' for Fahrenheit.")
     
-    except ValueError as e:
-        print(f"Invalid temperature. {e}")
+    except ValueError:
+        print("Invalid temperature. Please enter a numeric value.")
 
 if __name__ == "__main__":
     main()
