e78d93db8074b2d02b9295e24d8de0ef7e851931
Esther Kibuu <kibuue@gmail.com>
2025-10-12 06:38:06 +0300

    Fixed an error in library_system.py

--- oop/library_system.py
+++ oop/library_system.py
@@ -13,6 +13,12 @@ class Book:
         """
         self.title = title
         self.author = author
+    
+    def __str__(self):
+        """
+        String representation of the Book.
+        """
+        return f"Book: {self.title} by {self.author}"
 
 class EBook(Book):
     """
@@ -30,6 +36,12 @@ class EBook(Book):
         """
         super().__init__(title, author)
         self.file_size = file_size
+    
+    def __str__(self):
+        """
+        String representation of the EBook.
+        """
+        return f"EBook: {self.title} by {self.author}, File Size: {self.file_size}KB"
 
 class PrintBook(Book):
     """
@@ -47,6 +59,12 @@ class PrintBook(Book):
         """
         super().__init__(title, author)
         self.page_count = page_count
+    
+    def __str__(self):
+        """
+        String representation of the PrintBook.
+        """
+        return f"PrintBook: {self.title} by {self.author}, Page Count: {self.page_count}"
 
 class Library:
     """
@@ -73,9 +91,4 @@ class Library:
         Print details of all books in the library.
         """
         for book in self.books:
-            if isinstance(book, EBook):
-                print(f"EBook: {book.title} by {book.author}, File Size: {book.file_size}KB")
-            elif isinstance(book, PrintBook):
-                print(f"PrintBook: {book.title} by {book.author}, Page Count: {book.page_count}")
-            else:
-                print(f"Book: {book.title} by {book.author}")
+            print(book)
